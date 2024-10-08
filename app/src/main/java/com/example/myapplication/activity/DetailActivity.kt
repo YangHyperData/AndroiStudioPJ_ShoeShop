08e97bcee7e63be47111d4d8594036726199aefd
YangHyperData <ntg0311@gmail.com>
2024-10-08 10:07:13 +0700

    shoeshop

---
+++ app/src/main/java/com/example/myapplication/activity/DetailActivity.kt
@@ -0,0 +1,90 @@
+package com.example.myapplication.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.Adapter.ColorAdapter
+import com.example.myapplication.Adapter.SizeAdapter
+import com.example.myapplication.Adapter.SliderAdapter
+import com.example.myapplication.Model.ItemsModel
+import com.example.myapplication.Model.SliderModel
+import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityDetailBinding
+import com.example.project1762.Helper.ManagmentCart
+
+class DetailActivity : BaseActivity() {
+    private lateinit var binding: ActivityDetailBinding
+    private lateinit var item: ItemsModel
+    private var numberOrder = 1
+    private lateinit var managmentCart: ManagmentCart
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        managmentCart = ManagmentCart(this)
+
+        getBundle()
+        banner()
+        initLists()
+    }
+
+    private fun initLists() {
+        val sizeList = ArrayList<String>()
+        for (size in item.size) {
+            sizeList.add(size.toString())
+        }
+
+        binding.sizeList.adapter = SizeAdapter(sizeList)
+        binding.sizeList.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        val colorList = ArrayList<String>()
+        for (imageUrl in item.picUrl) {
+            colorList.add(imageUrl)
+        }
+
+        binding.colorList.adapter = ColorAdapter(colorList)
+        binding.colorList.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+    }
+
+    private fun banner() {
+        val sliderItems = ArrayList<SliderModel>()
+        for (imageUrl in item.picUrl) {
+            sliderItems.add(SliderModel(imageUrl))
+        }
+
+        binding.slider.adapter = SliderAdapter(sliderItems, binding.slider)
+        binding.slider.clipToPadding = true
+        binding.slider.clipChildren = true
+        binding.slider.offscreenPageLimit = 1
+
+
+        if (sliderItems.size > 1) {
+            binding.dotIndicator.visibility = View.VISIBLE
+            binding.dotIndicator.attachTo(binding.slider)
+        }
+    }
+
+    private fun getBundle() {
+        item = intent.getParcelableExtra("object")!!
+
+        binding.titleTxt.text = title
+        binding.descriptionTxt.text = item.description
+        binding.priceTxt.text = "$" + item.price
+        binding.ratingTxt.text = "${item.rating} Rating"
+        binding.addToCartBtn.setOnClickListener {
+            item.numberInCart = numberOrder
+            managmentCart.insertFood(item)
+        }
+        binding.backBtn.setOnClickListener { finish() }
+        binding.cartBtn.setOnClickListener {
+        startActivity(Intent(this@DetailActivity,CartActivity::class.java))
+        }
+    }
+}
